3c07a1a34fc9bd06ce84b22de00f0d87fa45f555
Avinash Kumar <avinashmadhukar4@gmail.com>
2025-02-08 17:44:11 +0530

    some changes

--- public/script.js
+++ public/script.js
@@ -1,18 +1,51 @@
 const socket = io("http://localhost:5500");
-let userEmail = "";
 
-// Show chatbox and hide login/signup form
+// Show chat container and hide auth section after login
 const showChat = () => {
   document.getElementById("auth-container").classList.add("hidden");
   document.getElementById("chat-container").classList.remove("hidden");
 };
 
-// Show login/signup form and hide chatbox
+// Show login/signup again and hide chat on logout
 const showAuth = () => {
   document.getElementById("auth-container").classList.remove("hidden");
   document.getElementById("chat-container").classList.add("hidden");
 };
 
+// const signup = async () => {
+//   const email = document.getElementById("email").value;
+//   const password = document.getElementById("password").value;
+
+//   const res = await fetch("http://localhost:5500/signup", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({ username: email.split("@")[0], email, password }),
+//   });
+
+//   const data = await res.json();
+//   alert(data.message);
+//   if (res.ok) showChat();
+// };
+
+// const login = async () => {
+//   const email = document.getElementById("email").value;
+//   const password = document.getElementById("password").value;
+
+//   const res = await fetch("http://localhost:5500/login", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({ email, password }),
+//   });
+
+//   const data = await res.json();
+//   alert(data.message);
+//   if (res.ok) {
+//     showChat();
+//     socket.emit("join", email);
+//   }
+// };
+
+// Attach functions to the window object
 window.signup = async () => {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
@@ -25,10 +58,7 @@ window.signup = async () => {
 
   const data = await res.json();
   alert(data.message);
-  if (res.ok) {
-    userEmail = email;
-    showChat();
-  }
+  if (res.ok) showChat();
 };
 
 window.login = async () => {
@@ -44,15 +74,28 @@ window.login = async () => {
   const data = await res.json();
   alert(data.message);
   if (res.ok) {
-    userEmail = email;
     showChat();
     socket.emit("join", email);
   }
 };
 
+window.logout = () => {
+  alert("You have been logged out!");
+  showAuth();
+};
+
+window.sendMessage = () => {
+  const messageInput = document.getElementById("message");
+  const message = messageInput.value.trim();
+
+  if (message) {
+    socket.emit("message", { message });
+    addMessageToChat({ email: "You", message }, true);
+    messageInput.value = "";
+  }
+};
 const logout = () => {
   alert("You have been logged out!");
-  userEmail = "";
   showAuth();
 };
 
@@ -61,21 +104,14 @@ const sendMessage = () => {
   const message = messageInput.value.trim();
 
   if (message) {
-    // Emit message to the server
-    socket.emit("message", { email: userEmail, message });
-
-    // Add message to chatbox as "You"
+    socket.emit("message", { message });
     addMessageToChat({ email: "You", message }, true);
     messageInput.value = "";
   }
 };
 
-// Listen for incoming messages
 socket.on("message", (data) => {
-  if (data.email !== userEmail) {
-    // Show sender's email for others
-    addMessageToChat(data, false);
-  }
+  addMessageToChat(data, false);
 });
 
 function addMessageToChat(data, isSelf) {
